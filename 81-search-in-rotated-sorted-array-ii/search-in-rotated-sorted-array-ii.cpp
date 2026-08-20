6b0fa5144f8528b312be47307d3efa95a3e938e1
Tanmay Shankar <120157654+tanmays2k6@users.noreply.github.com>
2026-08-20 22:19:51 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (8.40%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool search(vector<int>& arr, int k) {
+        int n=arr.size();
+        int low=0, high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]==k) return true;
+            if(arr[low]==arr[mid] && arr[mid]==arr[high]){
+                low++; high--;
+                continue;
+            }
+            if(arr[low]<=arr[mid]){
+                if(arr[low]<=k && k<=arr[mid]){
+                    high=mid-1;
+                } else{
+                    low=mid+1;
+                }
+            } else{
+                if(arr[mid]<=k && k<=arr[high]){
+                    low=mid+1;
+                } else{
+                    high=mid-1;
+                }
+            }
+        }
+        return false;
+    }
+    
+};
